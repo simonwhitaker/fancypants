5385f05185521cf11a0beab304e4b664d9b39052
Simon Whitaker <sw@netcetera.org>
2010-04-03 14:04:43 +0100

    Added TreeMap class and treemap_in_element convenience method

--- javascript/fancypants.js
+++ javascript/fancypants.js
@@ -15,6 +15,22 @@ function Frame (label, value, origin, area) {
     this.area = area;
 }
 
+function TreeMap() {
+    this.frames = [];
+}
+
+TreeMap.prototype.push = function(data) {
+    this.frames.push(data);
+}
+
+TreeMap.prototype.concat = function(data) {
+    if (data instanceof TreeMap) {
+        this.frames = this.frames.concat(data.frames);
+    } else {
+        throw "Argument to TreeMap.concat must be a TreeMap object";
+    }
+}
+
 function BaseData() { }
 
 BaseData.prototype.get_total = function() {
@@ -121,7 +137,7 @@ Dataset.prototype.treemap = function(area, origin, padding, threshold, flat) {
     var w = area.width;
     var h = area.height;
     
-    var result = [];
+    var result = new TreeMap();
     
     for (var i = 0; i < this.data.length; i++) {
         var datum = this.data[i];
@@ -182,7 +198,7 @@ Dataset.prototype.treemap = function(area, origin, padding, threshold, flat) {
         } else {
             var tmap = datum.treemap(area, origin, padding, threshold, flat);
             if (flat) {
-                result = result.concat(tmap);
+                result.concat(tmap);
             } else {
                 result.push(tmap);
             }
@@ -190,4 +206,47 @@ Dataset.prototype.treemap = function(area, origin, padding, threshold, flat) {
     }
     
     return result;
+}
+
+Dataset.prototype.treemap_in_element = function(element_id, padding, threshold) {
+    var element = document.getElementById(element_id);
+    var w       = parseInt(window.getComputedStyle(element, null).getPropertyValue('width'));
+    var h       = parseInt(window.getComputedStyle(element, null).getPropertyValue('height'));
+    var area    = new Rect(w, h);
+    var origin  = new Point(0, 0);
+    
+    if (padding == null) {
+        padding = 0;
+    }
+    if (threshold == null) {
+        threshold = 0;
+    }
+    
+    var tm      = this.treemap(area, origin, padding, threshold, true);
+    
+    for (var i = 0; i < tm.frames.length; i++) {
+      var frame = tm.frames[i];
+      var div = document.createElement('div');
+      div.style.position = "absolute";
+      div.className     = "frame " + frame.label.replace('.', ' ');
+      div.style.width   = frame.area.width;
+      div.style.height  = frame.area.height;
+      div.style.left    = frame.origin.x;
+      div.style.top     = frame.origin.y;
+      element.appendChild(div);
+      
+      var content = document.createElement('div');
+      content.className = 'content';
+      div.appendChild(content);
+
+      var val = document.createElement('div');
+      val.className = 'value';
+      val.innerHTML = frame.value;
+      content.appendChild(val);
+
+      var lbl = document.createElement('div');
+      lbl.className = 'label';
+      lbl.innerHTML = frame.label;
+      content.appendChild(lbl);
+    }
 }
